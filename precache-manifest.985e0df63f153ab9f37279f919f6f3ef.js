132ecabe1b6196d6d307d9b3c6f3859c6291885d
Mohammed Zoher Guniem <2918474@uis.no>
2020-07-06 14:35:55 +0200

    Updates

---
+++ precache-manifest.985e0df63f153ab9f37279f919f6f3ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae30d1ee4c5e222308981df76956336c",
+    "url": "/spatium/index.html"
+  },
+  {
+    "revision": "3da365caced9af2b7ab7",
+    "url": "/spatium/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "c44f2128a27d06c69166",
+    "url": "/spatium/static/css/main.be7872c8.chunk.css"
+  },
+  {
+    "revision": "3da365caced9af2b7ab7",
+    "url": "/spatium/static/js/2.75c0ee88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spatium/static/js/2.75c0ee88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44f2128a27d06c69166",
+    "url": "/spatium/static/js/main.9827068d.chunk.js"
+  },
+  {
+    "revision": "684dec0296c159079743",
+    "url": "/spatium/static/js/runtime-main.ab65cd31.js"
+  }
+]);
